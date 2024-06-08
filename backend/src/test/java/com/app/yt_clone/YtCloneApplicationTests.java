3ca3c6034faa9fae35afbdb3bc46c332af55f14c
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-06-08 04:04:50 +0200

    Project initialization

---
+++ backend/src/test/java/com/app/yt_clone/YtCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.yt_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
